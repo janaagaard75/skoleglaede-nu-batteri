00ea6b0805e63b503e0f2c0c4316a1e8863b3037
Jan Aagaard <jan@aagaard.net>
2024-10-20 20:48:24 +0200

    Invert if

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -14,12 +14,14 @@ export function ExternalLink({ href, ...rest }: LinkPropsWithStringHref) {
       {...rest}
       href={href}
       onPress={async event => {
-        if (Platform.OS !== "web") {
+        if (Platform.OS === "web") {
+          return;
+        }
+
           // Prevent the default behavior of linking to the default browser on native.
           event.preventDefault();
           // Open the link in an in-app browser.
           await openBrowserAsync(href);
-        }
       }}
     />
   );
