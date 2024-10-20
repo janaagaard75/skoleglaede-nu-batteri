aa15184fc994314d8299816eba948e3ae58a97b1
Jan Aagaard <jan@aagaard.net>
2024-10-20 21:21:51 +0200

    Remove unused tab-related things

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -32,10 +32,6 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen
-          name="(tabs)"
-          options={{ headerShown: false }}
-        />
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
