914c82f8b95329e44cca997cc411ffb72f982621
Jan Aagaard <jan@aagaard.net>
2024-10-23 19:53:05 +0200

    Sort

--- qr-codes/generate.ts
+++ qr-codes/generate.ts
@@ -2,22 +2,22 @@ import * as fs from "fs";
 import * as QRCode from "qrcode";
 
 const qrCodes = [
-  "+005pp",
-  "+010pp",
-  "+020pp",
-  "+030pp",
-  "+040pp",
-  "+050pp",
   "-005pp",
   "-010pp",
   "-020pp",
   "-030pp",
   "-040pp",
   "-050pp",
-  "+heart",
-  "-heart",
-  "+flame",
   "-flame",
+  "-heart",
+  "+005pp",
+  "+010pp",
+  "+020pp",
+  "+030pp",
+  "+040pp",
+  "+050pp",
+  "+flame",
+  "+heart",
 ];
 
 fs.mkdirSync("out", { recursive: true });
