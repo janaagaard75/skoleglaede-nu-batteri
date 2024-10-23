760bf48e6cbdbe0f4aafa68b9bde4132f1d33c9f
Jan Aagaard <jan@aagaard.net>
2024-10-23 23:02:53 +0200

    Show a camera

--- components/ScannerDialog.tsx
+++ components/ScannerDialog.tsx
@@ -1,5 +1,47 @@
+import { CameraView, useCameraPermissions } from "expo-camera";
+import { Button, View } from "react-native";
 import { ThemedText } from "./themed/ThemedText";
 
 export const ScannerDialog = () => {
-  return <ThemedText>Awesome 🎉</ThemedText>;
+  const [cameraPermissions, requestCameraPermissions] = useCameraPermissions();
+
+  if (cameraPermissions === null) {
+    return (
+      <View>
+        <ThemedText>Venter på tilladelse til kameraet&hellip;</ThemedText>
+      </View>
+    );
+  }
+
+  if (!cameraPermissions.granted) {
+    return (
+      <View>
+        <ThemedText
+          style={{
+            marginTop: 20,
+            textAlign: "center",
+          }}
+        >
+          Vi har brug for din tilladelse til at bruge kameraet.
+        </ThemedText>
+        <Button
+          onPress={requestCameraPermissions}
+          title="Giv adgang til kameraet"
+        />
+      </View>
+    );
+  }
+
+  return (
+    <View>
+      <CameraView facing="back">
+        <View
+          style={{
+            backgroundColor: "transparent",
+            margin: 64,
+          }}
+        ></View>
+      </CameraView>
+    </View>
+  );
 };
