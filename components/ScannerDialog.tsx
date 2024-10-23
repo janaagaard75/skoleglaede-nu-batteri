757d43645d40a06d334af4fc8d11a95f83275d95
Jan Aagaard <jan@aagaard.net>
2024-10-23 23:26:56 +0200

    Green box

--- components/ScannerDialog.tsx
+++ components/ScannerDialog.tsx
@@ -3,17 +3,22 @@ import {
   CameraView,
   useCameraPermissions,
 } from "expo-camera";
+import { BarCodeBounds } from "expo-camera/build/legacy/Camera.types";
 import { useState } from "react";
 import { Button, View } from "react-native";
 import { ThemedText } from "./themed/ThemedText";
 import { ThemedView } from "./themed/ThemedView";
 
 export const ScannerDialog = () => {
+  const [bounds, setBounds] = useState<BarCodeBounds | undefined>(undefined);
   const [cameraPermissions, requestCameraPermissions] = useCameraPermissions();
-  const [scannedBarcode, setScannedBarcode] = useState<string>("");
+  const [scannedBarcode, setScannedBarcode] = useState<string | undefined>(
+    undefined
+  );
 
   const barcodeScanned = (scanningResult: BarcodeScanningResult) => {
     setScannedBarcode(scanningResult.data);
+    setBounds(scanningResult.bounds);
   };
 
   if (cameraPermissions === null) {
@@ -40,24 +45,40 @@ export const ScannerDialog = () => {
   }
 
   return (
-    <ThemedView>
+    <ThemedView style={{ height: "100%" }}>
       <CameraView
         barcodeScannerSettings={{
           barcodeTypes: ["qr"],
         }}
         facing="back"
         onBarcodeScanned={barcodeScanned}
+        style={{
+          height: 20 * 16,
+          width: 20 * 10,
+          marginLeft: "auto",
+          marginRight: "auto",
+        }}
+        zoom={0.003}
       >
-        <View
-          style={{
-            backgroundColor: "transparent",
-            margin: 64,
-          }}
-        ></View>
+        {bounds !== undefined && (
+          <View
+            style={[
+              {
+                borderColor: "green",
+                borderWidth: 2,
+                position: "absolute",
+              },
+              {
+                height: bounds.size.height,
+                left: bounds.origin.x,
+                top: bounds.origin.y,
+                width: bounds.size.width,
+              },
+            ]}
+          />
+        )}
       </CameraView>
-      <ThemedText>
-        {scannedBarcode === "" ? "Ingenting" : scannedBarcode}
-      </ThemedText>
+      <ThemedText>{scannedBarcode ?? "Ingenting"}</ThemedText>
     </ThemedView>
   );
 };
