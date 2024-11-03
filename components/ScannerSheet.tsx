b2216d9cf4dc79290ce3bbdfa9e460415aea7d5a
Jan Aagaard <jan@aagaard.net>
2024-11-03 22:14:14 +0100

    Show after summary

--- components/ScannerSheet.tsx
+++ components/ScannerSheet.tsx
@@ -2,6 +2,7 @@ import { useCameraPermissions } from "expo-camera";
 import React, { useState } from "react";
 import { View } from "react-native";
 import { Battery } from "./Battery";
+import { getNewValues } from "./getNewValues";
 import { FlameIcon } from "./iconsRow/FlameIcon";
 import { FlameOutlineIcon } from "./iconsRow/FlameOutlineIcon";
 import { HeartIcon } from "./iconsRow/HeartIcon";
@@ -154,6 +155,8 @@ const ScannedCodeFeedback = ({
     );
   }
 
+  const newValues = getNewValues({ flames, hearts, percentage }, qrCode);
+
   return (
     <View
       style={{
@@ -162,54 +165,11 @@ const ScannedCodeFeedback = ({
         height: "100%",
       }}
     >
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-        }}
-      >
-        <View
-          style={{
-            alignItems: "center",
-          }}
-        >
-          <View
-            style={{
-              alignContent: "center",
-              marginLeft: 20,
-              width: 80,
-            }}
-          >
-            <Battery percentage={percentage} />
-          </View>
-          <View
-            style={{
-              height: 5,
-            }}
-          />
-          <IconsRow
-            currentValue={flames}
-            excludedIcon={<FlameOutlineIcon />}
-            gap={1}
-            includedIcon={<FlameIcon />}
-            maximum={10}
-            size={12}
-          />
-          <View
-            style={{
-              height: 5,
-            }}
-          />
-          <IconsRow
-            currentValue={hearts}
-            excludedIcon={<HeartOutlineIcon />}
-            gap={1}
-            includedIcon={<HeartIcon />}
-            maximum={10}
-            size={12}
-          />
-        </View>
-      </View>
+      <Summary
+        flames={flames}
+        hearts={hearts}
+        percentage={percentage}
+      />
       <View
         style={{
           width: 40,
@@ -219,11 +179,70 @@ const ScannedCodeFeedback = ({
       >
         <ThemedText>&#x21E8;</ThemedText>
       </View>
-      <View
-        style={{
-          flex: 1,
-        }}
-      ></View>
+      <Summary
+        flames={newValues.newFlames}
+        hearts={newValues.newHearts}
+        percentage={newValues.newPercentage}
+      />
     </View>
   );
 };
+
+const Summary = ({
+  flames,
+  hearts,
+  percentage,
+}: {
+  flames: number;
+  hearts: number;
+  percentage: number;
+}) => (
+  <View
+    style={{
+      flex: 1,
+      justifyContent: "center",
+    }}
+  >
+    <View
+      style={{
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          alignContent: "center",
+          marginLeft: 20,
+          width: 80,
+        }}
+      >
+        <Battery percentage={percentage} />
+      </View>
+      <View
+        style={{
+          height: 5,
+        }}
+      />
+      <IconsRow
+        currentValue={flames}
+        excludedIcon={<FlameOutlineIcon />}
+        gap={1}
+        includedIcon={<FlameIcon />}
+        maximum={10}
+        size={12}
+      />
+      <View
+        style={{
+          height: 5,
+        }}
+      />
+      <IconsRow
+        currentValue={hearts}
+        excludedIcon={<HeartOutlineIcon />}
+        gap={1}
+        includedIcon={<HeartIcon />}
+        maximum={10}
+        size={12}
+      />
+    </View>
+  </View>
+);
