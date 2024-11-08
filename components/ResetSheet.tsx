0125259ef2d7452ca7630466eec54e2229ad3979
Jan Aagaard <jan@aagaard.net>
2024-11-08 21:09:03 +0100

    Mirror the confirm design from the scanner sheet and add a battery icon

--- components/ResetSheet.tsx
+++ components/ResetSheet.tsx
@@ -1,3 +1,5 @@
+import { View } from "react-native";
+import { Battery } from "./battery/Battery";
 import { SlideToConfirm } from "./SlideToConfirm";
 import { ThemedText } from "./themed/ThemedText";
 import { ThemedView } from "./themed/ThemedView";
@@ -10,20 +12,43 @@ export const ResetSheet = (props: Props) => {
   return (
     <ThemedView
       style={{
+        display: "flex",
         height: "100%",
-        marginHorizontal: 30,
         gap: 30,
-        marginTop: 30,
       }}
     >
-      <ThemedText>
+      <ThemedText
+        style={{
+          marginTop: 30,
+          marginHorizontal: 30,
+        }}
+      >
         Bekræft at du vil nulstille til 20% og fjerne alle hjerter og flammer.
       </ThemedText>
-      <SlideToConfirm
-        disabled={false}
-        onConfirm={props.onReset}
-        title="Bekræft &nbsp;&#x21E8;"
-      />
+      <View
+        style={{
+          alignSelf: "center",
+          alignItems: "center",
+          width: 200,
+        }}
+      >
+        <Battery percentage={20} />
+      </View>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "flex-end",
+          marginHorizontal: "auto",
+          marginBottom: 60,
+          width: 270,
+        }}
+      >
+        <SlideToConfirm
+          disabled={false}
+          onConfirm={props.onReset}
+          title="Bekræft &nbsp;&#x21E8;"
+        />
+      </View>
     </ThemedView>
   );
 };
