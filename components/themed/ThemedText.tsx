0667947a829b39ba608248ae420559f72ae1cf4a
Jan Aagaard <jan@aagaard.net>
2024-11-23 21:44:25 +0100

    Remove unused type property

--- components/themed/ThemedText.tsx
+++ components/themed/ThemedText.tsx
@@ -1,11 +1,9 @@
-import { StyleSheet, Text, type TextProps } from "react-native";
+import { Text, type TextProps } from "react-native";
 import { useColors } from "../colors/useColors";
 
-type Props = TextProps & {
-  type?: "default" | "title" | "defaultSemiBold" | "subtitle" | "link";
-};
+type Props = TextProps;
 
-export function ThemedText({ style, type = "default", ...rest }: Props) {
+export function ThemedText({ style, ...otherProps }: Props) {
   const colors = useColors();
 
   return (
@@ -13,41 +11,12 @@ export function ThemedText({ style, type = "default", ...rest }: Props) {
       style={[
         {
           color: colors.text,
+          fontSize: 20,
+          lineHeight: 30,
         },
-        type === "default" ? styles.default : undefined,
-        type === "defaultSemiBold" ? styles.defaultSemiBold : undefined,
-        type === "link" ? styles.link : undefined,
-        type === "subtitle" ? styles.subtitle : undefined,
-        type === "title" ? styles.title : undefined,
         style,
       ]}
-      {...rest}
+      {...otherProps}
     />
   );
 }
-
-const styles = StyleSheet.create({
-  default: {
-    fontSize: 20,
-    lineHeight: 30,
-  },
-  defaultSemiBold: {
-    fontSize: 16,
-    lineHeight: 24,
-    fontWeight: "600",
-  },
-  link: {
-    lineHeight: 30,
-    fontSize: 16,
-    color: "#0a7ea4",
-  },
-  subtitle: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: "bold",
-    lineHeight: 32,
-  },
-});
