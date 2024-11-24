90daf4327ff276458ebb27fe55c5f829264a7368
Jan Aagaard <jan@aagaard.net>
2024-11-24 21:57:31 +0100

    Remove adaptive icon

--- app.config.ts
+++ app.config.ts
@@ -3,10 +3,6 @@ import { ConfigContext, ExpoConfig } from "expo/config";
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   android: {
-    adaptiveIcon: {
-      backgroundColor: "#ffffff",
-      foregroundImage: "./assets/icon-light.png",
-    },
     package: "com.janaagaard75.skoleglaedenubatteri",
     permissions: ["android.permission.CAMERA"],
   },
