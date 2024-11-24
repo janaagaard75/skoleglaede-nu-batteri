b7c9c3322942b2334f766ea0b5ac5e983a496ee0
Jan Aagaard <jan@aagaard.net>
2024-11-24 21:40:15 +0100

    Use TypeScript

--- app.config.js
+++ app.config.js
@@ -1,69 +1,69 @@
-export default {
-  expo: {
-    android: {
-      adaptiveIcon: {
-        backgroundColor: "#ffffff",
-        foregroundImage: "./assets/icon-light.png",
-      },
-      package: "com.janaagaard75.skoleglaedenubatteri",
-      permissions: ["android.permission.CAMERA"],
+import { ConfigContext, ExpoConfig } from "expo/config";
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  android: {
+    adaptiveIcon: {
+      backgroundColor: "#ffffff",
+      foregroundImage: "./assets/icon-light.png",
     },
-    experiments: {
-      typedRoutes: true,
-    },
-    extra: {
-      eas: {
-        projectId: "4445138c-8d76-4f8e-a92c-e6e56c81924c",
-      },
-    },
-    icon: "./assets/icon-light.png",
-    ios: {
-      bundleIdentifier: "com.janaagaard75.skoleglaedenubatteri",
-      config: {
-        usesNonExemptEncryption: false,
-      },
-      entitlements: {
-        "aps-environment": "development",
-      },
-      supportsTablet: true,
-    },
-    name: "Skoleglæde.nu Batteri",
-    newArchEnabled: true,
-    orientation: "portrait",
-    plugins: [
-      [
-        "expo-camera",
-        {
-          cameraPermission:
-            "Tillad at $(PRODUCT_NAME) får adgang til kameraet.",
-          recordAudioAndroid: false,
-        },
-      ],
-      "expo-font",
-      "expo-router",
-      "expo-secure-store",
-      [
-        "expo-splash-screen",
-        {
-          backgroundColor: "#ffffff",
-          image: "./assets/icon-light.png",
-          dark: {
-            image: "./assets/icon-dark.png",
-            backgroundColor: "#000000",
-          },
-          imageWidth: 200,
-        },
-      ],
-    ],
-    runtimeVersion: {
-      policy: "appVersion",
-    },
-    scheme: "myapp",
-    slug: "skoleglaede-nu-batteri",
-    userInterfaceStyle: "automatic",
-    updates: {
-      url: "https://u.expo.dev/4445138c-8d76-4f8e-a92c-e6e56c81924c",
-    },
-    version: "1.0.0",
+    package: "com.janaagaard75.skoleglaedenubatteri",
+    permissions: ["android.permission.CAMERA"],
   },
-};
+  experiments: {
+    typedRoutes: true,
+  },
+  extra: {
+    eas: {
+      projectId: "4445138c-8d76-4f8e-a92c-e6e56c81924c",
+    },
+  },
+  icon: "./assets/icon-light.png",
+  ios: {
+    bundleIdentifier: "com.janaagaard75.skoleglaedenubatteri",
+    config: {
+      usesNonExemptEncryption: false,
+    },
+    entitlements: {
+      "aps-environment": "development",
+    },
+    supportsTablet: true,
+  },
+  name: "Skoleglæde.nu Batteri",
+  newArchEnabled: true,
+  orientation: "portrait",
+  plugins: [
+    [
+      "expo-camera",
+      {
+        cameraPermission: "Tillad at $(PRODUCT_NAME) får adgang til kameraet.",
+        recordAudioAndroid: false,
+      },
+    ],
+    "expo-font",
+    "expo-router",
+    "expo-secure-store",
+    [
+      "expo-splash-screen",
+      {
+        backgroundColor: "#ffffff",
+        image: "./assets/icon-light.png",
+        dark: {
+          image: "./assets/icon-dark.png",
+          backgroundColor: "#000000",
+        },
+        imageWidth: 200,
+      },
+    ],
+  ],
+  runtimeVersion: {
+    policy: "appVersion",
+  },
+  scheme: "myapp",
+  slug: "skoleglaede-nu-batteri",
+  userInterfaceStyle: "automatic",
+  updates: {
+    url: "https://u.expo.dev/4445138c-8d76-4f8e-a92c-e6e56c81924c",
+  },
+  version: "1.0.0",
+});
