52f6e6e4c1d2949cdb7ec28462d115dfa51ff054
Jan Aagaard <jan@aagaard.net>
2024-11-24 21:38:10 +0100

    Use JS syntax

--- app.config.js
+++ app.config.js
@@ -1,42 +1,43 @@
-{
-  "expo": {
-    "android": {
-      "adaptiveIcon": {
-        "backgroundColor": "#ffffff",
-        "foregroundImage": "./assets/icon-light.png"
+export default {
+  expo: {
+    android: {
+      adaptiveIcon: {
+        backgroundColor: "#ffffff",
+        foregroundImage: "./assets/icon-light.png",
       },
-      "package": "com.janaagaard75.skoleglaedenubatteri",
-      "permissions": ["android.permission.CAMERA"]
+      package: "com.janaagaard75.skoleglaedenubatteri",
+      permissions: ["android.permission.CAMERA"],
     },
-    "experiments": {
-      "typedRoutes": true
+    experiments: {
+      typedRoutes: true,
     },
-    "extra": {
-      "eas": {
-        "projectId": "4445138c-8d76-4f8e-a92c-e6e56c81924c"
-      }
-    },
-    "icon": "./assets/icon-light.png",
-    "ios": {
-      "bundleIdentifier": "com.janaagaard75.skoleglaedenubatteri",
-      "config": {
-        "usesNonExemptEncryption": false
+    extra: {
+      eas: {
+        projectId: "4445138c-8d76-4f8e-a92c-e6e56c81924c",
       },
-      "entitlements": {
-        "aps-environment": "development"
-      },
-      "supportsTablet": true
     },
-    "name": "Skoleglæde.nu Batteri",
-    "newArchEnabled": true,
-    "orientation": "portrait",
-    "plugins": [
+    icon: "./assets/icon-light.png",
+    ios: {
+      bundleIdentifier: "com.janaagaard75.skoleglaedenubatteri",
+      config: {
+        usesNonExemptEncryption: false,
+      },
+      entitlements: {
+        "aps-environment": "development",
+      },
+      supportsTablet: true,
+    },
+    name: "Skoleglæde.nu Batteri",
+    newArchEnabled: true,
+    orientation: "portrait",
+    plugins: [
       [
         "expo-camera",
         {
-          "cameraPermission": "Tillad at $(PRODUCT_NAME) får adgang til kameraet.",
-          "recordAudioAndroid": false
-        }
+          cameraPermission:
+            "Tillad at $(PRODUCT_NAME) får adgang til kameraet.",
+          recordAudioAndroid: false,
+        },
       ],
       "expo-font",
       "expo-router",
@@ -44,25 +45,25 @@
       [
         "expo-splash-screen",
         {
-          "backgroundColor": "#ffffff",
-          "image": "./assets/icon-light.png",
-          "dark": {
-            "image": "./assets/icon-dark.png",
-            "backgroundColor": "#000000"
+          backgroundColor: "#ffffff",
+          image: "./assets/icon-light.png",
+          dark: {
+            image: "./assets/icon-dark.png",
+            backgroundColor: "#000000",
           },
-          "imageWidth": 200
-        }
-      ]
+          imageWidth: 200,
+        },
+      ],
     ],
-    "runtimeVersion": {
-      "policy": "appVersion"
+    runtimeVersion: {
+      policy: "appVersion",
     },
-    "scheme": "myapp",
-    "slug": "skoleglaede-nu-batteri",
-    "userInterfaceStyle": "automatic",
-    "updates": {
-      "url": "https://u.expo.dev/4445138c-8d76-4f8e-a92c-e6e56c81924c"
+    scheme: "myapp",
+    slug: "skoleglaede-nu-batteri",
+    userInterfaceStyle: "automatic",
+    updates: {
+      url: "https://u.expo.dev/4445138c-8d76-4f8e-a92c-e6e56c81924c",
     },
-    "version": "1.0.0"
-  }
-}
+    version: "1.0.0",
+  },
+};
