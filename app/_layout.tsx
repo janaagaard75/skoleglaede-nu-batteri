5ce4e413d501b603c6da961008d31d4eaa7e6ad7
Jan Aagaard <jan@aagaard.net>
2024-12-03 20:57:41 +0100

    We don't use the font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,33 +6,13 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
 import "react-native-reanimated";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const colors = useColors();
-
-  const [fontsLoaded] = useFonts({
-    SpaceMono: require("../assets/SpaceMono-Regular.ttf"),
-  });
-
-  useEffect(() => {
-    if (fontsLoaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
-
-  if (!fontsLoaded) {
-    return <></>;
-  }
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
