5588efaf5c9cb5f46b803825c62989670f790212
Jan Aagaard <jan@aagaard.net>
2025-08-26 21:03:31 +0200

    Remove the problematic and unused 'style' property

--- src/themed/ThemedTextButton.tsx
+++ src/themed/ThemedTextButton.tsx
@@ -2,9 +2,10 @@ import { Pressable, View, type PressableProps } from "react-native";
 import { useColors } from "../colors/useColors";
 import { ThemedText } from "./ThemedText";
 
-type Props = PressableProps & React.RefAttributes<View> & { children: string };
+type Props = Omit<PressableProps, "style">
+  & React.RefAttributes<View> & { children: string };
 
-export const ThemedTextButton = ({ children, style, ...otherProps }: Props) => {
+export const ThemedTextButton = ({ children, ...otherProps }: Props) => {
   const colors = useColors();
 
   return (
@@ -18,8 +19,6 @@ export const ThemedTextButton = ({ children, style, ...otherProps }: Props) => {
           paddingHorizontal: 16,
           paddingVertical: 4,
         },
-        // @ts-expect-error - Don't know why TSC doesn't accept using 'style' here.
-        style,
       ]}
       {...otherProps}
     >
