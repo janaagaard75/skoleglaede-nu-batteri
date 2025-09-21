7cf95e251671949a4474f1585c63918cb0ccdf82
Jan Aagaard <jan@aagaard.net>
2025-09-21 21:17:31 +0200

    Ignore lint error when colors arrays are the same

--- src/colors/useThemeColor.ts
+++ src/colors/useThemeColor.ts
@@ -2,6 +2,7 @@ import { Colors } from "@/src/colors/Colors";
 import { useColorScheme } from "react-native";
 
 export const useThemeColor = (
+  // eslint-disable-next-line @typescript-eslint/no-duplicate-type-constituents
   colorName: keyof typeof Colors.dark & keyof typeof Colors.light,
 ) => {
   const theme = useColorScheme() ?? "light";
