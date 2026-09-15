fadcece2a4d320855966802be1d37a60cd885098
Jan Aagaard <jan@aagaard.net>
2026-09-15 21:32:13 +0200

    Render scanner overlays outside CameraView

--- src/scannerScreen/Viewfinder.tsx
+++ src/scannerScreen/Viewfinder.tsx
@@ -66,26 +66,30 @@ export const Viewfinder = (props: Props) => {
   };
 
   return (
-    <CameraView
-      barcodeScannerSettings={{
-        barcodeTypes: ["qr"],
-      }}
-      facing="back"
-      onBarcodeScanned={qrCodeScanned}
+    <View
       style={{
-        backgroundColor: colors.disabledText,
         height: viewfinderSize,
         marginLeft: "auto",
         marginRight: "auto",
         width: viewfinderSize,
       }}
     >
+      <CameraView
+        barcodeScannerSettings={{ barcodeTypes: ["qr"] }}
+        facing="back"
+        onBarcodeScanned={qrCodeScanned}
+        style={{
+          backgroundColor: colors.disabledText,
+          height: "100%",
+          width: "100%",
+        }}
+      />
       <HeadUpDisplay
         scannerMargin={scannerMargin}
         viewfinderSize={viewfinderSize}
       />
       <QrCodeHighlighter bounds={bounds} />
-    </CameraView>
+    </View>
   );
 };
 
